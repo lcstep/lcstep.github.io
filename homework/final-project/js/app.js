cdd9b31443318e3c1fe6f80211550938c560712e
Laura Stephenson <lcstep@gmail.com>
2018-05-01 14:27:17 -0400

    final project

--- homework/final-project/js/app.js
+++ homework/final-project/js/app.js
@@ -15,3 +15,23 @@ function hideVideo(e) {
 $(".video").hover(function(event){
 	$(event.currentTarget).find(".caption").toggle();
 })
+
+$("#plus").click(zoomIn);
+$("#minus").click(zoomOut);
+
+function zoomIn () {
+	$("main").css("transform", "scale(1.2)");
+}
+
+function zoomOut () {
+	$("main").css("transform", "scale(.8)");
+}
+
+
+// var scrollerObj = new Scroller(function(left, top, zoom) {
+// 	// apply coordinates/zooming
+// }, {
+// 	scrollingY: false
+// });
+
+// scrollerObj.setDimensions(1000, 1000, 3000, 3000);
