1975748bfd81a171e1fe64eb96e639d08caaa620
lcstep <32037330+lcstep@users.noreply.github.com>
2018-05-06 22:22:50 -0400

    bike bonus

--- homework/citi_bike_api/js/script.js
+++ homework/citi_bike_api/js/script.js
@@ -10,50 +10,110 @@
 // When I visit the site
 // I expect to see fifty stations with free bikes only
 
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://api.citybik.es/citi-bike-nyc.json',
+//   success: function(stations) {
+//     console.log(stations)
+
+//    stations.forEach(freeBikes)
+//     function freeBikes(station) {
+//       if (station.free > 0) {
+//         return true
+//       }
+//     }
+//     var hasBikes = stations.filter(freeBikes);
+//     console.log(hasBikes)
+//     var shortlist = hasBikes.splice(0,50);
+//     console.log(shortlist)
+    
+    
+//     shortlist.forEach(wrapStation);
+//     function wrapStation(station) {
+      
+//       var singles = "<li>" + station.name + "</li>"
+
+      
+//       $("#container ul").append(singles)
+//     }
+
+// }
+// });
+
+
+
 // Bonus
 
 // As a User
 // I want to get data from the newer API instead: https://api.citybik.es/v2/networks/citi-bike-nyc
+
 // So that I can get the most accurate results
 
+
+$.ajax({
+  type: 'GET',
+  url: 'https://api.citybik.es/v2/networks/citi-bike-nyc',
+  success: function(citi) {
+    console.log(citi)
+
+
+    var stations = citi.network.stations;
+    console.log(stations)
+
+    stations.forEach(freeBikes)
+    function freeBikes(station) {
+      if (station.free_bikes > 0) {
+        return true
+      }
+    }
+    var hasBikes = stations.filter(freeBikes);
+    console.log(hasBikes)
+   
+    
+      $("#bikes").change(function(){
+        $("#container ul").empty();
+        var bikeNum = parseInt($("select").val());
+        console.log(bikeNum)
+
+        hasBikes.forEach(neededBikes)
+        function neededBikes(station){
+          if (station.free_bikes > bikeNum) {
+            return true
+          }
+
+        }
+
+        var enoughBikes = hasBikes.filter(neededBikes);
+        console.log(enoughBikes);
+
+         var shortlist = enoughBikes.splice(0,50);
+        console.log(shortlist)
+         shortlist.forEach(wrapStation);
+    function wrapStation(station) {
+      
+      var singles = "<li>" + station.name + "</li>"
+
+      
+      $("#container ul").append(singles);
+    }
+    });
+
+    // shortlist.forEach(wrapStation);
+    // function wrapStation(station) {
+      
+  
+    // }
+
+}
+});
+
+
+  
 // Unreal, Super Epic Bonus
 
 // As a User
 // When I select "number of free bikes" from a dropdown
 // I see fifty stations where the "number of free bikes" is greater than my selection
 
-$.ajax({
-  type: 'GET',
-  url: 'https://api.citybik.es/v2/networks/citi-bike-nyc',
-  xhrFields: { withCredentials: false },
-  crossDomain: false,
-  success: function(stations) {
-    console.log(stations)
-
-   stations.forEach(freeBikes)
-    function freeBikes(station) {
-      if (station.free > 0) {
-        return true
-      }
-    }
-    var hasBikes = stations.filter(freeBikes);
-    console.log(hasBikes)
-    var shortlist = hasBikes.splice(0,50);
-    console.log(shortlist)
-    
-    
-    shortlist.forEach(wrapStation);
-    function wrapStation(station) {
-    	
-    	var singles = "<li>" + station.name + "</li>"
-
-    	
-    	$("#container ul").append(singles)
-    }
-
-   
-  
-}
-});
 
 
--- homework/google_maps_example/js/app.js
+++ homework/google_maps_example/js/app.js
@@ -35,6 +35,7 @@
         var newhaven = {lat: 41.3083, lng: -72.9279	};
         var madison = {lat: 41.2960, lng: -72.5754	};
         var wallingford = {lat: 41.4570, lng: -72.8231	};
+
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 8,
           center: newyork,
@@ -46,19 +47,60 @@
         });
         var marker2 = new google.maps.Marker({
           position: newhaven,
-          map: map
+          map: map,
+          "visible": false
         });
         var marker3 = new google.maps.Marker({
           position: madison,
-          map: map
+          map: map,
+          "visible": false
         });
-        var marker3 = new google.maps.Marker({
+        var marker4 = new google.maps.Marker({
           position: wallingford,
           map: map,
-          display: none
-
+          "visible": false
         });
+       
+         
 
-        $("")
-
+       
+$("button").click(function() {
+          marker2.setVisible(true);
+          marker3.setVisible(true);
+          marker4.setVisible(true); // maps API hide call
+        });
       }
+
+
+$.ajax({
+  type: 'GET',
+  url: 'https://api.citybik.es/v2/networks/citi-bike-nyc',
+  success: function(stations) {
+    console.log(stations)
+
+   stations.forEach(freeBikes)
+    function freeBikes(station) {
+      if (station.free > 0) {
+        return true
+      }
+    }
+    var hasBikes = stations.filter(freeBikes);
+    console.log(hasBikes)
+    var shortlist = hasBikes.splice(0,50);
+    console.log(shortlist)
+    
+    
+    shortlist.forEach(wrapStation);
+    function wrapStation(station) {
+      
+      var singles = "<li>" + station.name + "</li>"
+
+      
+      $("#container ul").append(singles)
+    }
+
+   
+  
+}
+});
+
