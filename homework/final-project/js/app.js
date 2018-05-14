1c80f4c88461c9544be3244d0eaf00adaa5b2cb7
lcstep <32037330+lcstep@users.noreply.github.com>
2018-05-13 20:06:44 -0400

    final project

--- homework/final-project/js/app.js
+++ homework/final-project/js/app.js
@@ -1,6 +1,8 @@
 
+
 $(".intro").click(function(){
 	$(".intro").fadeOut("slow");
+	$("header").css("opacity", "1")
 })
 
 // // VIDEOS DON'T OPEN WHILE DRAGGING
@@ -8,43 +10,39 @@ $(".intro").click(function(){
 $(".video").click(function(e) {
 	e.preventDefault();
 });
- 
 
 
-
-
+// DRAGGABLE & MOMENTUM
 
 var element = document.querySelector('.draggable');
-var container = document.querySelector('.wrapper');
+var container = document.querySelector('.container');
 var draggable = new momentum.Draggable(element, {
-	container: wrapper,
-  friction: 0.085,
-  offsetFriction: 0.1
+	elementBounds: 'parent',
+	friction: 0.065,
+	offsetFriction: 0.1
+	
 });
 
 
 
 
 
-// // HOVER TO PLAY VIDEO
+// // HOVER TO PLAY VIDEO and show captions
 
 $(".video").mouseenter(hoverVideo);
 $(".video").mouseleave(hideVideo);
 
 function hoverVideo(e) {  
-     // $(event.currentTarget).find(".caption").css("opacity", "1");
-    $('video', this).get(0).play(); 
-    
+     // $(event.currentTarget).find(".caption").show();
+     $('video', this).get(0).play(); 
+
+ }
+
+ function hideVideo(e) {
+	// $(event.currentTarget).find(".caption").hide();
+	$('video', this).get(0).pause(); 
+
 }
-
-function hideVideo(e) {
-	// $(event.currentTarget).find(".caption").css("opacity", "0");
-    $('video', this).get(0).pause(); 
-  
-}
-
-
-// // SHOW CAPTIONS
 
 
 
@@ -54,65 +52,17 @@ $("#plus").click(zoomIn);
 $("#minus").click(zoomOut);
 
 function zoomIn () {
-	var current = parseFloat($('main').css('transform').split(',')[3]);
+	var current = parseFloat($('#video-wrapper').css('transform').split(',')[3]);
 	var zoom = current * 1.2;	
-	$("main").css("transform", "scale(" + zoom + ")");
+	$("#video-wrapper").css({"transform": "scale(" + zoom + ")", "transform-origin": "center"});
+	
 }
 
 function zoomOut () {
-	var current = parseFloat($('main').css('transform').split(',')[3]);
+	var current = parseFloat($('#video-wrapper').css('transform').split(',')[3]);
 	var zoomOut = current * .8;
 	console.log(zoomOut);
-	$("main").css("transform", "scale(" + zoomOut + ")");
+	$("#video-wrapper").css({"transform": "scale(" + zoomOut + ")", "transform-origin": "center"});
+
+
 }
-
-// // DRAGGABLE
-
-// $( function() {
-//     $( "body" ).draggable();
-//   } );
-
-
-// // MOMENTUM
-
- // var myImpetus = new Impetus({
-	// 	source: 'main',
-	// 	initialValues: [0,0],
-	// 	friction: .93,
-	// 	update: function(x, y) {
-	// 		this.style.left = x + 'px';
-	// 		this.style.top = y + 'px';
-	// 	}
-
-	// });
-
-// MOMENTUM ALTERNATE
-
-
- 
-// // sun
-
-// $(document).mousemove(function(e){
-//     $(".sun").css({
-//       left:e.pageX - 1000, 
-//       top:e.pageY - 1000 
-//     });
-
-// });
-
-
-
-// // SCROLL ZOOM
-
-// // $(window).scroll(function() {
-// //   var scroll = $(window).scrollTop();
-// // 	$("main").css({
-// // 			transform: 'scale('+(100 - scroll/20)/100+')'
-// // 		// translateX('+(100 - scroll/100)/100+'%) 
-// // 		//Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
-// // 		//"-webkit-filter": "blur(" + (scroll/200) + "px)",
-// // 		//filter: "blur(" + (scroll/200) + "px)"
-// // 	});
-// // 	var width = $("main").width();
-// // 			console.log(width);
-// // });
---
+++ homework/final-project/js/old.js
@@ -0,0 +1,51 @@
+
+// // DRAGGABLE
+
+// $( function() {
+//     $( "body" ).draggable();
+//   } );
+
+
+// // MOMENTUM
+
+ // var myImpetus = new Impetus({
+	// 	source: 'main',
+	// 	initialValues: [0,0],
+	// 	friction: .93,
+	// 	update: function(x, y) {
+	// 		this.style.left = x + 'px';
+	// 		this.style.top = y + 'px';
+	// 	}
+
+	// });
+
+// MOMENTUM ALTERNATE
+
+
+
+// // sun
+
+// $(document).mousemove(function(e){
+//     $(".sun").css({
+//       left:e.pageX - 1000, 
+//       top:e.pageY - 1000 
+//     });
+
+// });
+
+
+
+// // SCROLL ZOOM
+
+// // $(window).scroll(function() {
+// //   var scroll = $(window).scrollTop();
+// // 	$("main").css({
+// // 			transform: 'scale('+(100 - scroll/20)/100+')'
+// // 		// translateX('+(100 - scroll/100)/100+'%) 
+// // 		//Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
+// // 		//"-webkit-filter": "blur(" + (scroll/200) + "px)",
+// // 		//filter: "blur(" + (scroll/200) + "px)"
+// // 	});
+// // 	var width = $("main").width();
+// // 			console.log(width);
+// // });
