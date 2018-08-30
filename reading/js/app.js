86497dfd651224f55faefb3a3b864f8efdf5b41e
Laura Stephenson <lcstep@gmail.com>
2018-08-30 15:31:32 -0400

    grid responsive

--- reading/js/app.js
+++ reading/js/app.js
@@ -4,7 +4,7 @@ function showImages(el) {
         var thisPos = $(this).offset().top;
 
         var topOfWindow = $(window).scrollTop();
-        if (topOfWindow + windowHeight - 200 > thisPos ) {
+        if (topOfWindow + windowHeight - 100 > thisPos ) {
             $(this).addClass("fadeIn");
             
     
